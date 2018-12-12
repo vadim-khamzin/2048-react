eb19b6e0d0fb3ce23704388eda6ea4db40fa39e6
Vadim Khamzin <vkhamzin94@gmail.com>
2018-12-12 10:32:19 +0300

    Updates

---
+++ precache-manifest.c87be2f83fd15762102366b42801af01.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "415a33dcab67dce3d714",
+    "url": "/2048-react/static/js/runtime~main.415a33dc.js"
+  },
+  {
+    "revision": "173c4816654e9c29daf4",
+    "url": "/2048-react/static/js/main.173c4816.chunk.js"
+  },
+  {
+    "revision": "20692f63ae3843443261",
+    "url": "/2048-react/static/js/1.20692f63.chunk.js"
+  },
+  {
+    "revision": "173c4816654e9c29daf4",
+    "url": "/2048-react/static/css/main.26188000.chunk.css"
+  },
+  {
+    "revision": "967c54a6c4623f55085e3653ef1f59c4",
+    "url": "/2048-react/index.html"
+  }
+];
