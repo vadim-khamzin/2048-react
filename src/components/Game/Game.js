78057eb273c6c55d65a3cae2ca532ffb0877fba3
Vadim Khamzin <vkhamzin94@gmail.com>
2018-12-15 19:15:08 +0300

    Remove semicolons

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import PropTypes from 'prop-types';
+import PropTypes from 'prop-types'
 import { moveValues, addRandomValue } from './gameLogic'
 
 class Game extends Component {
